62e485e84e964d730ac9a240dff0353e1aede5b6
v-cheye <v-cheye@microsoft.com>
2018-09-12 18:06:34 +0800

    update test case

--- tests/test_iotedgemodule.py
+++ tests/test_iotedgemodule.py
@@ -4,11 +4,12 @@ class TestIoTEdgeModule():
     def test_RequiredFileExists(self):
         print(self.__class__)
         assert 1 == 1
-		
-	def test_RequiredFileExists2(self):
+
+    def test_RequiredFileExists2(self):
         print(self.__class__)
         assert 1 == 1
 
 if __name__ == '__main__':
     iotEdgeModule=TestIoTEdgeModule()
     iotEdgeModule.test_RequiredFileExists()
+    iotEdgeModule.test_RequiredFileExists2()
