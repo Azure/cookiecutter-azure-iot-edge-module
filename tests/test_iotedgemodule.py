6e4cb257ddfbcd880f45aaa375fcc43235c85d96
v-cheye <v-cheye@microsoft.com>
2018-09-17 11:39:22 +0800

    update code by comments

--- tests/test_iotedgemodule.py
+++ tests/test_iotedgemodule.py
@@ -1,3 +1,5 @@
+import json
+import logging
 import os
 import pytest
 
@@ -5,23 +7,28 @@ root_dir = os.getcwd()
 module_name = "iotedgeFilterModule"
 file_list = ["requirements.txt", "module.json", "main.py", "Dockerfile.arm32v7",
              "Dockerfile.amd64.debug", "Dockerfile.amd64", ".gitignore"]
+image_repository="localhost:5000/"+module_name
 
 
 @pytest.fixture(scope="module")
 def test_install_iotedgemodule(request):
-    installmodulecommand = "cookiecutter --no-input https://github.com/Azure/cookiecutter-azure-iot-edge-module module_name=" + \
-        module_name + " image_repository=localhost:5000/"+module_name+" --checkout master"
+    installmodulecommand = "cookiecutter --no-input "+os.environ["BUILD_REPOSITORY_LOCALPATH"]+" module_name=" + \
+        module_name + " image_repository="+image_repository
+    print("Cookiecutter execution command: "+installmodulecommand)
     pipe = os.popen(installmodulecommand)
     yield pipe.readlines()
     pipe.close()
 
 
-@pytest.fixture(scope="module", params=file_list)
-def test_check_file_exist(request):
-    exists = os.path.exists(os.path.join(root_dir, module_name, request.param))
-    return exists
+def test_check_file_exist(test_install_iotedgemodule):
+    for file_name in file_list:
+        file_path = os.path.join(root_dir, module_name, file_name)
+        print("Check file: "+file_path)
+        assert os.path.exists(file_path) == True
 
-
-def test_check_iotedgemodule_exist(test_install_iotedgemodule, test_check_file_exist):
-    if not test_check_file_exist:
-        raise Exception("IoT Edge Module cannot be found.")
+def test_check_module_json_context():
+    module_file_path=os.path.join(root_dir, module_name, "module.json")
+    with open(module_file_path) as module_file:
+        module_file_content=json.load(module_file)
+    
+    assert image_repository in module_file_content["image"]["repository"]
