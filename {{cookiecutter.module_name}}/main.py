d3d0f8a8722532cbd8b4e7701a6c598ba6cae8ec
Chaoyi Yuan <blackchoey@users.noreply.github.com>
2019-10-12 14:14:04 +0800

    Cherry pick f8dfe34

--- {{cookiecutter.module_name}}/main.py
+++ {{cookiecutter.module_name}}/main.py
@@ -2,93 +2,74 @@
 # Licensed under the MIT license. See LICENSE file in the project root for
 # full license information.
 
-import random
 import time
+import os
 import sys
-import iothub_client
-# pylint: disable=E0611
-from iothub_client import IoTHubModuleClient, IoTHubClientError, IoTHubTransportProvider
-from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError
+import asyncio
+from six.moves import input
+import threading
+from azure.iot.device.aio import IoTHubModuleClient
 
-# messageTimeout - the maximum time in milliseconds until a message times out.
-# The timeout period starts at IoTHubModuleClient.send_event_async.
-# By default, messages do not expire.
-MESSAGE_TIMEOUT = 10000
-
-# global counters
-RECEIVE_CALLBACKS = 0
-SEND_CALLBACKS = 0
-
-# Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
-PROTOCOL = IoTHubTransportProvider.MQTT
-
-# Callback received when the message that we're forwarding is processed.
-def send_confirmation_callback(message, result, user_context):
-    global SEND_CALLBACKS
-    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
-    map_properties = message.properties()
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
-    SEND_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
-
-
-# receive_message_callback is invoked when an incoming message arrives on the specified 
-# input queue (in the case of this sample, "input1").  Because this is a filter module, 
-# we will forward this message onto the "output1" queue.
-def receive_message_callback(message, hubManager):
-    global RECEIVE_CALLBACKS
-    message_buffer = message.get_bytearray()
-    size = len(message_buffer)
-    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode('utf-8'), size) )
-    map_properties = message.properties()
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
-    RECEIVE_CALLBACKS += 1
-    print ( "    Total calls received: %d" % RECEIVE_CALLBACKS )
-    hubManager.forward_event_to_output("output1", message, 0)
-    return IoTHubMessageDispositionResult.ACCEPTED
-
-
-class HubManager(object):
-
-    def __init__(
-            self,
-            protocol=IoTHubTransportProvider.MQTT):
-        self.client_protocol = protocol
-        self.client = IoTHubModuleClient()
-        self.client.create_from_environment(protocol)
-
-        # set the time until a message times out
-        self.client.set_option("messageTimeout", MESSAGE_TIMEOUT)
-        
-        # sets the callback when a message arrives on "input1" queue.  Messages sent to 
-        # other inputs or to the default will be silently discarded.
-        self.client.set_message_callback("input1", receive_message_callback, self)
-
-    # Forwards the message received onto the next stage in the process.
-    def forward_event_to_output(self, outputQueueName, event, send_context):
-        self.client.send_event_async(
-            outputQueueName, event, send_confirmation_callback, send_context)
-
-def main(protocol):
+async def main():
     try:
-        print ( "\nPython %s\n" % sys.version )
+        if not sys.version >= "3.5.3":
+            raise Exception( "The sample requires python 3.5.3+. Current version of Python: %s" % sys.version )
         print ( "IoT Hub Client for Python" )
 
-        hub_manager = HubManager(protocol)
+        # The client object is used to interact with your Azure IoT hub.
+        module_client = IoTHubModuleClient.create_from_edge_environment()
 
-        print ( "Starting the IoT Hub Python sample using protocol %s..." % hub_manager.client_protocol )
-        print ( "The sample is now waiting for messages and will indefinitely.  Press Ctrl-C to exit. ")
+        # connect the client.
+        await module_client.connect()
 
-        while True:
-            time.sleep(1)
+        # define behavior for receiving an input message on input1
+        async def input1_listener(module_client):
+            while True:
+                input_message = await module_client.receive_message_on_input("input1")  # blocking call
+                print("the data in the message received on input1 was ")
+                print(input_message.data)
+                print("custom properties are")
+                print(input_message.custom_properties)
+                print("forwarding mesage to output1")
+                await module_client.send_message_to_output(input_message, "output1")
 
-    except IoTHubError as iothub_error:
-        print ( "Unexpected error %s from IoTHub" % iothub_error )
-        return
-    except KeyboardInterrupt:
-        print ( "IoTHubModuleClient sample stopped" )
+        # define behavior for halting the application
+        def stdin_listener():
+            while True:
+                try:
+                    selection = input("Press Q to quit\n")
+                    if selection == "Q" or selection == "q":
+                        print("Quitting...")
+                        break
+                except:
+                    time.sleep(10)
 
-if __name__ == '__main__':
-    main(PROTOCOL)
+        # Schedule task for C2D Listener
+        listeners = asyncio.gather(input1_listener(module_client))
+
+        print ( "The sample is now waiting for messages. ")
+
+        # Run the stdin listener in the event loop
+        loop = asyncio.get_event_loop()
+        user_finished = loop.run_in_executor(None, stdin_listener)
+
+        # Wait for user to indicate they are done listening for messages
+        await user_finished
+
+        # Cancel listening
+        listeners.cancel()
+
+        # Finally, disconnect
+        await module_client.disconnect()
+
+    except Exception as e:
+        print ( "Unexpected error %s " % e )
+        raise
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+
+    # If using Python 3.7 or above, you can use following code instead:
+    # asyncio.run(main())
